f7011079b8828ec2f02a92733dc25d89313417ff
ramonabroberg <>
2023-10-02 22:08:07 +0000

    Create build_board function, print_board function and main so it now shows the player's board

--- run.py
+++ run.py
@@ -1,19 +1,40 @@
 from random import randint
 
+size = 6
+number_ships = 5
+
+
 def new_game():
     """
     Starts a new game. Sets the size and number of ships for the game.
     Informs about how the game works and asks the player for its name.
     """
-    size = 6
-    number_ships = 5
-    print("^" * 30)
+    global player_name
+    print("^" * 40)
     print("Welcome to the Battleship Game!")
-    print(f"You have {number_ships} ships to find on the board. The boardsize is {size}x{size}.")
+    print(f"You have {number_ships} ships to find on the board. Boardsize: {size}x{size}.")
     print(f"Available options are row: 1-{size} and column: 1-{size}.")
-    print("^" * 30)
-    player_name = input("What is your name?")
-    print("Nice to meet you " + player_name + ", let's play a game!")
+    print("^" * 40)
+    player_name = input("What is your name? ")
+    print("Nice to meet you " + player_name + ", let's play a game!\n")
+    return player_name
 
-new_game()
+
+def build_board(size):
+    return [["O" for count in range(size)] for count in range(size)]
+
+
+def print_board(board):
+    print(f"{player_name}'s board:")
+    for i in board:
+        print(*i)
+
+
+def main():
+    new_game()
+    board = build_board(size)
+    print_board(board)
+
+
+main()
 

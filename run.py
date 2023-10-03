9677eb8010bea69676cd1b28db726947c3f7bf87
ramonabroberg <>
2023-10-03 11:25:08 +0000

    Create the functions random_player_values and random_computer_values. Separate print_board to print_player_board and print_computer_board. Update them with the random guesses to test it.

--- run.py
+++ run.py
@@ -1,19 +1,20 @@
 from random import randint
 
+# Sets the size for the boards and the amount of ships.
 size = 6
 number_ships = 5
 
 
 def new_game():
     """
-    Starts a new game. Sets the size and number of ships for the game.
+    Starts a new game.
     Informs about how the game works and asks the player for its name.
     """
     global player_name
     print("^" * 40)
     print("Welcome to the Battleship Game!")
     print(f"You have {number_ships} ships to find on the board.")
-    print("Boardsize: {size}x{size}.")
+    print(f"Boardsize: {size}x{size}.")
     print(f"Available options are row: 1-{size} and column: 1-{size}.")
     print("^" * 40)
     player_name = input("What is your name? ")
@@ -21,43 +22,43 @@ def new_game():
     print("^" * 40)
 
 
+def random_player_values(size):
+    global player_row
+    global player_column
+    player_row, player_column = randint(0, size - 1), randint(0, size - 1)
+
+
+def random_computer_values(size):
+    global computer_row
+    global computer_column
+    computer_row, computer_column = randint(0, size - 1), randint(0, size - 1)
+
+
 def build_board(size):
     return [["O" for count in range(size)] for count in range(size)]
 
 
-def print_player_board(board):
+def print_player_board(board, ship_row, ship_column):
     print(f"{player_name}'s board:")
     for i in board:
         print(*i)
+    print(f"{player_name}'s guess: {player_row}, {player_column}")
 
 
-def print_computer_board(board):
+def print_computer_board(board, ship_row, ship_column):
     print("\nComputer's board:")
     for i in board:
         print(*i)
-
-
-def random_row(size):
-    return randint(0, size - 1)
-
-
-def random_column(size):
-    return randint(0, size - 1)
-
-
-def build_ships():
-    pass
-
-
-def player_guess():
-    pass
+    print(f"Computer's guess: {computer_row}, {computer_column}")
 
 
 def main():
     new_game()
     board = build_board(size)
-    print_player_board(board)
-    print_computer_board(board)
+    random_player_values(size)
+    random_computer_values(size)
+    print_player_board(board, player_row, player_column)
+    print_computer_board(board, computer_row, computer_column)
 
 
 main()
